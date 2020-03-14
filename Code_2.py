12c9f5cce7d870c1bf0dc128381b680a079830f7
r-chaudhary <r.chaudhary@outlook.in>
2020-03-14 17:20:51 +0530

    Code_2 Added.

---
+++ Code_2.py
@@ -0,0 +1,12 @@
+# Introduction to Python
+# Programmer : Rahul Chaudhary
+# Email : r.chaudhary@outlook.in
+
+# Code : 2
+# Python Program to check Even and Odd Number.
+
+num = int(input("Enter number : "))
+if num % 2 == 0:
+    print("%d is Even Number"%num)
+else:
+    print("%d is Odd Number"%num)
