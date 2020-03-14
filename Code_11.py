04472f64913460ccd842d7f9b57c1454288b13e5
r-chaudhary <r.chaudhary@outlook.in>
2020-03-14 17:41:34 +0530

    Code_11 Added.

---
+++ Code_11.py
@@ -0,0 +1,13 @@
+# Introduction to Python
+# Programmer : Rahul Chaudhary
+# Email : r.chaudhary@outlook.in
+
+# Code : 11
+# Python Program to print list element less than 5.
+
+list = [1,1,2,3,5,8,13,21,34,55,89]
+print("less than 5")
+for i in list:
+    if i < 5:
+        print(i, end = " ")
+        
